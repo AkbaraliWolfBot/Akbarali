e2c61c0216fbe0b1c3836e55d58d5d4722767544
AkbaraliWolfBot <61541271+AkbaraliWolfBot@users.noreply.github.com>
2020-05-19 16:31:40 +0200

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,31 @@
+// Select color input
+// Select size input
+var height, width, color;
+$(document).ready(function() {
+    $("#sizePicker").submit(function(event) {
+        event.preventDefault();
+        height = $("#inputHeight").val();
+        width = $("#inputWidth").val();
+        makeGrid(height, width);
+        console.log("height = " + height + " and width = " + width);
+
+    })
+});
+// When size is submitted by the user, call makeGrid()
+
+
+function makeGrid(x, y) {
+    $("tr").remove();
+    // Your code goes here!
+    for (var i = 1; i <= x; i++) {
+        $("#pixelCanvas").append("<tr id=table" + i + "></tr>");
+        for (var j = 1; j <= y; j++) {
+            $("#table" + i).append("<td></td>");
+        }
+    }
+    // add color to cell 
+    $("td").click(function(element) {
+        color = $("#colorPicker").val();
+        $(event.target).css("background-color", color);
+    });
+}
