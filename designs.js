d249b236cd5351c0c701aa9354f9921cff91bba9
Akbarali <akbarali2191@gmail.com>
2020-09-20 14:51:35 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -22,7 +22,7 @@ function makeGrid(x, y) {
             $("#table" + i).append("<td></td>");
         }
     }
-    // adding color to cell
+    // adding color to cell by click
     $("td").click(function(element) {
         color = $("#colorPicker").val();
         $(event.target).css("background-color", color);
